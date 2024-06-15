9edfb1e5a031a9e476864cebb090d7b136d69e99
0num4 <r.oonuma@matsuri-tech.com>
2024-06-15 16:02:29 +0900

    feat: update index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
 console.log("Hello via Bun!");
+
+try {
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello, ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log('The event payload: ', payload);
+} catch (error) {
+    const e = error as Error;
+    core.setFailed(e.message);
+}

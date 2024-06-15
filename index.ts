d01b33575555a35b946ab92c6f2169c0d13048d9
0num4 <r.oonuma@matsuri-tech.com>
2024-06-15 16:33:19 +0900

    feat: repoを表示するように

--- index.ts
+++ index.ts
@@ -1,9 +1,26 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
-console.log("Hello via Bun!");
+console.log("Hello via Bun2!");
+
+
 
 try {
     const nameToGreet = core.getInput('who-to-greet');
+    const token = core.getInput('github-pat-token');
+    const inputBadges = core.getInput('badges');
+    const badgeStyle = core.getInput('badge-style');
+    const octokit = github.getOctokit(token);
+    const repos = await octokit.paginate(octokit.rest.repos.listForAuthenticatedUser, {
+        per_page: 100,
+        affiliation: "owner",
+    });
+    const totalRepos = repos.length;
+    console.log(`Total repos: ${totalRepos}`);
+    for (const repo of repos) {
+        console.log(`Repo: ${repo.name}`);
+    }
+
+
     console.log(`Hello, ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
